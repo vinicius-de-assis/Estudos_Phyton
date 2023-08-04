7e499452fa9a6378c59ec25a968222edb4349e57
Vinícius de Assis Madruga Prestes <104784274+vinicius-de-assis@users.noreply.github.com>
2023-08-04 13:15:39 -0300

    test

--- 1 - Funções e Variáveis/hello_strings.py
+++ 1 - Funções e Variáveis/hello_strings.py
@@ -3,8 +3,14 @@
 #1- perguntar nome do usuario (usamos a variavel name para guardar o nome)
 name = input("What´s your name? ")
 
-#remove os espaços em branco da string 
-name = name.strip()         #nome_da_string.strip()
+#FUNCAO --> nome_da_string.strip()     --> remove espaços
+name = name.strip()         
+#var = nome_da_string.strip()
+
+#FUNCAO --> nome_da_string.capitalize()     --> Coloca letra maíscula no primeiro caracter (ex: vini fica Vini)
+name = name.capitalize()         
+#var = nome_da_string.capitalize()
+
 
 #2- diz hello para o user
 print(f"hello, {name}")
